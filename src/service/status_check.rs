dd4b5a08d9e956e565cc9fba20a9678ca0eec6c2
Gerry Agbobada <gerry@fiberplane.com>
2022-11-17 11:52:22 +0100

    Move test to avoid creating single-use getter

--- src/service/status_check.rs
+++ src/service/status_check.rs
@@ -83,12 +83,6 @@ impl DataSourceCheckTask {
     pub(crate) fn name(&self) -> &Name {
         &self.name
     }
-
-    // Only used in tests
-    #[allow(dead_code)]
-    pub(crate) fn retries_left(&self) -> isize {
-        self.retries_left
-    }
 }
 
 /// The current state of a collection of data sources
@@ -120,4 +114,39 @@ impl DataSourcesStatusMap {
             data_sources: self.inner.values().cloned().collect(),
         }
     }
+}
+
+#[test]
+fn exponential_backoff_cap() {
+    fn test_rec(task: DataSourceCheckTask, old_delay: Duration, remaining_budget: Duration) {
+        if let Some((delay, new_task)) = task.next() {
+            assert!(
+                delay > old_delay,
+                "the new delay is longer than the old one."
+            );
+            let new_remaining_budget = remaining_budget.checked_sub(delay);
+            assert!(
+                new_remaining_budget.is_some(),
+                "The delay ({:?}) is bigger than the remaining budget {:?}",
+                delay,
+                remaining_budget
+            );
+            test_rec(new_task, delay, new_remaining_budget.unwrap());
+        }
+    }
+
+    fn test_case(total_duration: Duration, initial_delay: Duration, backoff_factor: f32) {
+        let task = DataSourceCheckTask::new(
+            Name::from_static("be-the-change"),
+            total_duration,
+            initial_delay,
+            backoff_factor,
+        );
+        assert!(task.retries_left >= 0, "At least 1 try will be attempted");
+        test_rec(task, Duration::from_secs(0), total_duration);
+    }
+
+    test_case(Duration::from_secs(300), Duration::from_secs(10), 1.5);
+    test_case(Duration::from_secs(300), Duration::from_secs(1000), 1.5);
+    test_case(Duration::from_secs(300), Duration::from_secs(300), 1.5);
 }
