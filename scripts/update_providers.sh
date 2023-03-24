66cd8cb5125e25c4ffab99d3f80d6e466bb4a3c7
Arend van Beelen jr <arendjr@gmail.com>
2023-03-24 17:38:06 +0100

    Revert to main + update providers

--- scripts/update_providers.sh
+++ scripts/update_providers.sh
@@ -10,43 +10,29 @@ PROVIDERS=(
 )
 
 REPO_ROOT=`dirname "$0"`/..
-FIBERPLANE_DIR="${REPO_ROOT}/../providers"
+PROVIDERS_DIR="${REPO_ROOT}/../providers"
 
 CYAN='\033[0;36m'
 WHITE='\033[0;37;1m'
 NC='\033[0m'
 
-which wasm-opt > /dev/null
-if [ $? -eq 1 ]; then
-    printf "Please make sure you have ${WHITE}wasm-opt${NC} installed and in your PATH.\n"
-    echo "Make sure to install a recent version from: "
-    echo "    https://github.com/WebAssembly/binaryen/releases"
-    exit 1
-fi
-
 set -e
 
-if [ ! -d $FIBERPLANE_DIR ]; then
+if [ ! -d $PROVIDERS_DIR ]; then
     echo "Please make sure you have the providers repository checked out next"
     echo "to your fpd project."
     exit 1
 fi
 
-cd $FIBERPLANE_DIR
+cd $PROVIDERS_DIR
 
 printf "${CYAN}Compiling providers...${NC}\n"
-for provider in "${PROVIDERS[@]}"; do
-    pushd "providers/${provider}"
-        cargo build --release
-    popd
-done
+cargo xtask build all
 
-pwd
-
-printf "${CYAN}Optimizing providers...${NC}\n"
 for provider in "${PROVIDERS[@]}"; do
-    # Optimize for performance
-    wasm-opt -Oz -c -o "../fpd/providers/${provider}.wasm" "target/wasm32-unknown-unknown/release/${provider}_provider.wasm"
+    OUTPUT="../fpd/providers/${provider}.wasm"
+    ARTIFACT="artifacts/${provider}.wasm"
+    cp $ARTIFACT $OUTPUT
 done
 
 echo "Done."
