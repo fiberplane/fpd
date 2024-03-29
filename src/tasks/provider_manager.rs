2283f6fe3d25c5bc5202b955bbd5deb424a133d6
Benno van den Berg <bennovandenberg@gmail.com>
2024-03-29 16:22:27 +0100

    Remove GitHub base url if it is present
    If the URI contains a 'authority' component, then octocrab will not add any credentials to the outgoing request. This is required for artifacts downloading

--- src/tasks/provider_manager.rs
+++ src/tasks/provider_manager.rs
@@ -181,14 +181,18 @@ async fn download_providers_tarball(
 ) -> Result<(), Error> {
     eprintln!("Downloading providers tarball from: {download_url}");
 
+    // We need to get rid of the base URL, to ensure that octocrab will add
+    // authentication headers again.
+    let download_url = download_url.trim_start_matches(GITHUB_BASE_URI);
+
     let response = octocrab._get(download_url).await?;
     let tarball_bytes = if response.status().is_redirection() {
         let header_value = response.headers().get("location").unwrap();
         let location = header_value.to_str().unwrap().to_string();
 
         if location.starts_with(GITHUB_BASE_URI) {
-            let uri = location.parse::<Uri>().unwrap();
-            let response = octocrab._get(uri).await?;
+            let location = location.trim_start_matches(GITHUB_BASE_URI);
+            let response = octocrab._get(location).await?;
             response.into_body().collect().await?.to_bytes()
         } else {
             reqwest::get(location).await?.bytes().await?
@@ -228,14 +232,18 @@ async fn download_providers_zip(
 ) -> Result<(), Error> {
     eprintln!("Downloading providers zip from: {download_url}");
 
+    // We need to get rid of the base URL, to ensure that octocrab will add
+    // authentication headers again.
+    let download_url = download_url.trim_start_matches(GITHUB_BASE_URI);
+
     let response = octocrab._get(download_url).await?;
     let zip_bytes = if response.status().is_redirection() {
         let header_value = response.headers().get("location").unwrap();
         let location = header_value.to_str().unwrap().to_string();
 
         if location.starts_with(GITHUB_BASE_URI) {
-            let uri = location.parse::<Uri>().unwrap();
-            let response = octocrab._get(uri).await?;
+            let location = location.trim_start_matches(GITHUB_BASE_URI);
+            let response = octocrab._get(location).await?;
             response.into_body().collect().await?.to_bytes()
         } else {
             reqwest::get(location).await?.bytes().await?
